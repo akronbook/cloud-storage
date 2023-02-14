7ee0d70894dcf0239b200ed669d2f5a5ea1b5742
Yuanming.Chen <akronbook@gmail.com>
2023-02-13 21:55:23 -0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,17 @@ const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+  define: {
+    global: {},
+  },
+  external: [
+    "AWS"
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
       name: 'cloud-storage',
       fileName: (format) => `cloud-storage.${format}.js`
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['AWS'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          AWS: 'AWS',
-        },
-      },
-    },
+    }
   }
 });
