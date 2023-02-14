80a5e0398f5bceb7353ced31f818ff518f6a3354
Yuanming.Chen <akronbook@gmail.com>
2023-02-13 21:35:53 -0700

    Try AWS global

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,26 @@ const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+  define: {
+    global: 'AWS'
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
       name: 'cloud-storage',
       fileName: (format) => `cloud-storage.${format}.js`
-    }
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['AWS'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'AWS',
+        },
+      },
+    },
   }
 });
